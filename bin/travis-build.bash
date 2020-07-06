90e7798903981c9a97b4f70142eb6258d5d4c5fc
william dutton <will.dutt@gmail.com>
2020-07-06 15:24:05 +1000

    make travis great again

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,15 +5,15 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
 git checkout $CKAN_BRANCH
 python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt
+pip install -r dev-requirements.txt 
 cd -
 
 echo "Creating the PostgreSQL user and database..."
